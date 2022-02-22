b982e2e6335fd266ecc2ba125343f9c3d72af356
SeokJingu <98717916+SeokJingu@users.noreply.github.com>
2022-02-23 04:50:50 +0900

    face

--- face.js
+++ face.js
@@ -24,6 +24,55 @@ function moon(size){
     scribble.scribbleEllipse( x, y, size, size );
 }      
 
+function face(style){
+  x = 1080 / 2;
+  y = 1080 / 2;
+  var scribble = new Scribble();
+  if (style == "egg"){
+    noFill();
+    strokeWeight( 5 );
+    scribble.scribbleEllipse(x,y, 500, 600)
+    scribble.scribbleEllipse(x,y, 500, 600)
+  }
+}
+
+function right_eye(style){
+  x = 648;
+  y = 432;
+  var scribble = new Scribble();
+  noFill();
+  strokeWeight( 5 );
+  scribble.scribbleEllipse(x,y, 20, 20)
+  scribble.scribbleEllipse(x,y, 80, 40)
+}
+
+function left_eye(style){
+  x = 432;
+  y = 432;
+  var scribble = new Scribble();
+  noFill();
+  strokeWeight( 5 );
+  scribble.scribbleEllipse(x,y, 20, 20)
+  scribble.scribbleEllipse(x,y, 80, 40)
+}
+
+function hair(style){
+  x = 432;
+  y = 270;
+  var scribble = new Scribble();
+  noFill();
+  strokeWeight( 5 );
+  scribble.scribbleLine(x,y, x_1=x+20, y_1=y+50)
+  scribble.scribbleLine(x_1,y_1, x = x_1+20, y = y_1-20)
+  scribble.scribbleLine(x,y, x_1=x+20, y_1=y+20)
+  scribble.scribbleLine(x_1,y_1, x = x_1+20, y = y_1-20)
+  scribble.scribbleLine(x,y, x_1=x+20, y_1=y+20)
+  scribble.scribbleLine(x_1,y_1, x = 1080/2, y = y_1-20)
+
+
+}
+
+
 function setup() {
     createCanvas( windowWidth = 1080, windowHeight = 1080 );
     background( 230 );
@@ -42,82 +91,14 @@ function setup() {
         line(grid_width*i, 0, grid_width*i, 1080);
         line(0, grid_height * i , 1080, grid_height * i);
     }
-    star5(100,10);
-    star5(50,15);
-    star5(20,20);
-    star5(10,25);
-
-    moon(200);
+    face("egg");
+    right_eye();
+    left_eye();
+    hair();
     
-    strokeWeight( 5 );
-    var xCoords = [ 150, 120, 400, 400, 1082-400, 1082-400, 1082-120,  1080-150 ];
-    var yCoords = [ 1080, 900, 850, 700, 700, 850, 900, 1080 ];
-    stroke( 255,0,0 );
-    scribble.scribbleFilling( xCoords, yCoords , 10,285 )
-    stroke( 128,64,0 );
-    strokeWeight( 1 );
-    noFill();
-    scribble.scribbleCurve( 0, 600, 1080, 600, 400, 500, 600, 500 );
-    stroke( 128,64,0 );
-    strokeWeight( 1 );
-    noFill();
-    scribble.scribbleCurve( 1, 600, 1079, 600, 400, 500, 600, 500 );
-    stroke( 128,64,0 );
-    strokeWeight( 1 );
-    noFill();
-    scribble.scribbleCurve( 2, 600, 1078, 600, 400, 500, 600, 500 );
-    stroke( 0,0,255 );
-
-
-
-    //star(1,1);
-    
-
-    // draw every value as a filled rect in a bar graph
-  /*  for ( var i = 0; i < values.length; i++ ) {
-      // calculate the x and y coordinates of the center of the rect and the height
-      var h = halfHeight * 0.01 * values[i];
-      var x = ( spacer + width ) * ( i + 1 ) - ( width / 2 );
-      var y = halfHeight - h / 2;
-      // set the thikness of the rect lines
-      strokeWeight( 5 );
-      // set the color of the rect lines to black
-      stroke( 0 );
-
-      // draw a rect for the value
-      scribble.scribbleRect( x, y, width, h );
-
-      // calculate the x and y coordinates for the border points of the hachure
-      var xleft   = x - width / 2 + 5;
-      var xright  = x + width / 2 - 5;
-      var ytop    = y - ( halfHeight *  0.01 * values[i] / 2 );
-      var ybottom = y + ( halfHeight *  0.01 * values[i] / 2 );
-      // reduce the sizes to fit in the rect
-      if ( ytop > ybottom ) {
-        ytop    -= 5;
-        ybottom += 5;
-      } else {
-        ytop    += 5;
-        ybottom -= 5;
-      }
-      // the x coordinates of the border points of the hachure
-      var xCoords = [ xleft, xright, xright, xleft ];
-      // the y coordinates of the border points of the hachure
-      var yCoords = [ ytop, ytop, ybottom, ybottom ];
-      // the gap between two hachure lines
-      var gap = 3.5;
-      // the angle of the hachure in degrees
-      var angle = 315;
-      // set the thikness of our hachure lines
-      strokeWeight( 3 );
-      //set the color of the hachure to a nice blue
-      stroke( 0, 50, 180 );
-
-      // fill the rect with a hachure
-      scribble.scribbleFilling( xCoords, yCoords , gap*2, angle );
-
-    }*/
+  
 }
+
 
 function draw() {
 
